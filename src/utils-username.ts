2014667c14c1fd429d6d1018d3057a17b67e8a5c
bluwy <bjornlu.dev@gmail.com>
2026-08-28 13:48:12 +0800

    Improve username detection

--- src/utils-username.ts
+++ src/utils-username.ts
@@ -1,8 +1,15 @@
 export function getUsername(authorEl: HTMLAnchorElement): string | undefined {
+  // If text content is empty, it might be just an image or something that links to the user,
+  // so we skip it
+  const fromText = authorEl.textContent?.trim()
+  if (fromText === '') return undefined
+
+  // If there's text content, we still check the href first to get the more accurate username,
+  // e.g. "/username" links
   const fromUrl = new URL(authorEl.href)
   const fromUrlPathnameParts = fromUrl.pathname.split('/')
   if (fromUrl.search === '' && fromUrlPathnameParts.length === 2) return fromUrlPathnameParts[1]
 
-  const fromText = authorEl.textContent?.trim()
-  if (fromText) return fromText
+  // Apply simple validation
+  if (fromText && /^[a-zA-Z0-9-]+$/.test(fromText)) return fromText
 }
